03f92a0b9e64d218f05743ca1ceea0002f70d5fd
VamsiKrishna <krishvamsi321@gmail.com>
2025-02-27 10:24:30 +0530

    Utility package

--- Modules/util.py
+++ Modules/util.py
@@ -1,9 +1,12 @@
 # Add your import statements here
-
-
-
-
-
+import nltk
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+from nltk.corpus import wordnet
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.tokenize import sent_tokenize 
 
 
 # Add any utility functions here
